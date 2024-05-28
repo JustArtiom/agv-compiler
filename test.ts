723d1289792b53c3df6bcb7cfcd896dd8f864860
justartiom <artiomsiatat@gmail.com>
2024-05-28 23:45:53 +0100

    rename

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import { tokenize } from "./lexer";
 import fs from "node:fs";
 
-for (let token of tokenize(fs.readFileSync("./code.gna", "utf-8"))) {
+for (let token of tokenize(fs.readFileSync("./code.agv", "utf-8"))) {
     console.log(token);
 }
